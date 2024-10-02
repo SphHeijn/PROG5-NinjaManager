f79645b6c75ee4e3db16e9709e04f73846d5098b
SphHeijn <sph.heijn@student.avans.nl>
2024-10-02 13:03:08 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG5_NinjaManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
